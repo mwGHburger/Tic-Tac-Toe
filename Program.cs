7f0c715c200c2e1378ab335e6e52c0196ef0c59b
Michael Wang <michael.wang@myob.com>
2020-09-17 12:45:58 +1000

    cool things

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace Tic_Tac_Toe
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // Run game
             TicTacToeGame.Run();
         }
     }
